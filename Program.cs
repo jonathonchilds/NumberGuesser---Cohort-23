96aafa132271cda356b8f19a8c72d60e6aca2182
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2021-11-25 17:02:17 -0500

    started else if formula for higher guess

--- Program.cs
+++ Program.cs
@@ -36,39 +36,43 @@ namespace NumberGuesserExplorerMode
         }
         static void GuessingLoop()
         {
-            var maxGuess = 1024;
-            var minGuess = 0;
+            var initialMaxGuess = 1024;
+            var initialMinGuess = 0;
+            var initialGuess = 512;
 
-            Console.WriteLine($"Is you number 512? Please type Y if so; otherwise type h " +
+            Console.WriteLine($"Is you number {initialGuess}? Please type Y if so; otherwise type h " +
                               "if your number is higher or type l if your number is lower.");
             var response = Console.ReadLine();
 
-            var affirmatives = new List<string>() { "y", "Y", "Yes", "yes", "YES", "YUS", "yep", "Yeah", "Yep" };
-            var higher = new List<string>() { "n", "N", "no", "NO", "No", "Nope", "nope", "Nerp", "nerp" };
-            var lower = new List<string>() { "l", "L", "Lower", "lower" };
+            var affirmativeUserResponse = new List<string>() { "y", "Y", "Yes", "yes", "YES", "YUS", "yep", "Yeah", "Yep" };
+            var negativeUserResponse = new List<string>() { "n", "N", "no", "NO", "No", "Nope", "nope", "Nerp", "nerp" };
+            var lowerGuess = new List<string>() { "l", "L", "Lower", "lower" };
             var highGuess = new List<string>() { "h", "H", "higher", "Higher" };
 
-            if (affirmatives.Contains(response))
+            if (affirmativeUserResponse.Contains(response))
             {
                 Console.WriteLine("Well that was easy! Only 1 guess!");
-                //     }
-                //     else if (higher.Contains(response))
-                //     {
-
-                //     }
-                //     else if (negat == "h")
-                //     {
-                //         var newGuess = maxGuess / 2;
-                //         Console.WriteLine($"Is {newGuess} your number?");
-                //     }
-                //     else if (response == "l")
-                //     {
-                //         var newGuess = minGuess / 2;
-                //         Console.WriteLine($"Is {newGuess} your number?");
-                //     }
-                //     else
-                //     {
-                //         Console.WriteLine("Please enter an acceptable response.");
+            }
+            else if (highGuess.Contains(response))
+            {
+                var newMinimumGuessValue = initialMaxGuess / 2;
+                var newGuess = (initialMaxGuess + newMinimumGuessValue) / 2;
+                initialGuess = newGuess;
+                Console.WriteLine();
+            }
+            // else if (negat == "h")
+            // {
+            //     var newGuess = maxGuess / 2;
+            //     Console.WriteLine($"Is {newGuess} your number?");
+            // }
+            // else if (response == "l")
+            // {
+            //     var newGuess = minGuess / 2;
+            //     Console.WriteLine($"Is {newGuess} your number?");
+            // }
+            else
+            {
+                Console.WriteLine("Please enter an acceptable response.");
             }
         }
         static void closingStatement()
