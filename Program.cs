872c6c740bfc65857665d0e2bea07ccd54bce5cd
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2021-11-29 14:05:03 -0500

    added additional while loop for "negative user responses"

--- Program.cs
+++ Program.cs
@@ -35,28 +35,33 @@ namespace NumberGuesserExplorerMode
             Console.WriteLine("Do not enter your number! Keep it in your head, please.");
             Console.WriteLine();
             Console.WriteLine("Now do me a favor and type any number that is higher than the number you've picked.");
+            Console.WriteLine();
             return Console.ReadLine();
         }
         public static void maxGuessesCalculator(ref int numberRangeMaximum, ref int numberRangeMinimum)
         {
             var maxGuesses = Math.Round(Math.Log2((numberRangeMaximum - numberRangeMinimum)) + 1);
-            Console.WriteLine($"This will only take me {maxGuesses} guesses to figure out, at most. ");
+            Console.WriteLine($"(This will only take me {maxGuesses} guesses to figure out, at most.)");
         }
 
         public static void ResponseLoop(ref int numberRangeMaximum, ref int numberRangeMinimum, ref int systemGuess, ref string userResponse)
         {
             // would love to import a text file containing every possible "yes" response; or, perhaps, just add radio buttons. That would be was easier. But the text file idea could apply for something like voice recognition
-            var affirmativeUserResponse = new List<string>() { "y", "Y", "Yes", "yes", "YES", "yasss", "YAS", "YUS", "yup", "yuppp", "yep", "Yeah", "Yep" };
+            var affirmativeUserResponse = new List<string>() { "y", "Y", "Yes", "yes", "YES", "yasss", "YAS", "YUS", "yup", "yuppp", "yep", "Yeah", "Yep", "you got it!", "that's it!" };
             var List_Of_Possible_Responses_For_Guess_Lower = new List<string>() { "l", "L", "Lower", "lower", "LOWER" };
             var List_Of_Possible_Responses_For_Guess_Higher = new List<string>() { "h", "H", "higher", "Higher", "HIGHER" };
+            var negativeUserResponse = new List<string>() { "n", "N", "no", "NO", "No", "Nope", "nope", "Nerp", "nerp" };
+            //how to make loop return to beginning while condition is user enters anything other than a provided list?
 
-            while (List_Of_Possible_Responses_For_Guess_Higher.Contains(userResponse))
+            while (!affirmativeUserResponse.Contains(userResponse))
             {
-                numberRangeMinimum = systemGuess;
-                systemGuess = (numberRangeMaximum + numberRangeMinimum) / 2;
-                Console.WriteLine($"Is your number {systemGuess}?");
-                userResponse = Console.ReadLine();
-
+                while (List_Of_Possible_Responses_For_Guess_Higher.Contains(userResponse))
+                {
+                    numberRangeMinimum = systemGuess;
+                    systemGuess = (numberRangeMaximum + numberRangeMinimum) / 2;
+                    Console.WriteLine($"Is your number {systemGuess}?");
+                    userResponse = Console.ReadLine();
+                }
                 while (List_Of_Possible_Responses_For_Guess_Lower.Contains(userResponse))
                 {
                     numberRangeMaximum = systemGuess;
@@ -64,20 +69,24 @@ namespace NumberGuesserExplorerMode
                     Console.WriteLine($"Is your number {systemGuess}?");
                     userResponse = Console.ReadLine();
                 }
-                if (affirmativeUserResponse.Contains(userResponse))
+                while (negativeUserResponse.Contains(userResponse))
                 {
-                    Console.WriteLine();
-                    Console.WriteLine("I'm sure you could do this too, but likely not as quickly. ☺️");
+                    Console.WriteLine("Well then tell me if it's higher or lower, please.");
+                    userResponse = Console.ReadLine();
                 }
-                // else
-                // {
-                //     Console.WriteLine("Please enter an acceptable response.");
-                // }
+            }
+            if (affirmativeUserResponse.Contains(userResponse))
+            {
+                Console.WriteLine();
+                Console.WriteLine("I'm sure you could do this too, but likely not as quickly. ☺️");
             }
         }
 
     }
 }
+
+
+
 
 //var negativeUserResponse = new List<string>() { "n", "N", "no", "NO", "No", "Nope", "nope", "Nerp", "nerp" };
 //_____________________________________________________________________________________________________________________
