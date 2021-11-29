83a5a040ad92bd3f9b0e11391cc8fe3e8d7ec969
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2021-11-28 23:08:56 -0500

    added lists for user responses (still bit buggy for unacceptable responses)

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ namespace NumberGuesserExplorerMode
         static void Main(string[] args)
         {
             var negativeUserResponse = new List<string>() { "n", "N", "no", "NO", "No", "Nope", "nope", "Nerp", "nerp" };
-            var lowerGuess = new List<string>() { "l", "L", "Lower", "lower", "LOWER" };
-            var ListOfPossibleResponsesForGuessHigher = new[] { "h", "H", "higher", "Higher", "HIGHER" };
+
+
 
             // var guessHigher = PossibleResponsesForGuessHigher(); //can i pass the string through? I gave this method a variable because it wouldn't allow me 
             // to pass the method directly through the NumberIsHigherLoop method.
@@ -19,7 +19,9 @@ namespace NumberGuesserExplorerMode
             var numberRangeMinimum = 0;
             var systemGuess = (numberRangeMaximum + numberRangeMinimum) / 2;
 
-            Console.WriteLine($@"Is you number {systemGuess}? Please type ""y"" if so, otherwise, type ""h"" if your number is higher or ""l"" if your number is lower.");
+            //var maxGuesses = Math.log((numberRangeMaximum - numberRangeMinimum) + 1);
+
+            Console.WriteLine($@"Is your number {systemGuess}? Please type ""y"" if so, otherwise, type ""h"" if your number is higher or ""l"" if your number is lower.");
             var userResponse = Console.ReadLine();
 
             ResponseLoop(ref numberRangeMaximum, ref numberRangeMinimum, ref systemGuess, ref userResponse);
@@ -28,43 +30,44 @@ namespace NumberGuesserExplorerMode
 
         public static void ResponseLoop(ref int numberRangeMaximum, ref int numberRangeMinimum, ref int systemGuess, ref string userResponse)
         {
+            // would love to import a text file containing every possible "yes" response; or, perhaps, just add radio buttons. That would be was easier. But the text file idea could apply for something like voice recognition
             var affirmativeUserResponse = new List<string>() { "y", "Y", "Yes", "yes", "YES", "yasss", "YAS", "YUS", "yup", "yuppp", "yep", "Yeah", "Yep" };
+            var List_Of_Possible_Responses_For_Guess_Lower = new List<string>() { "l", "L", "Lower", "lower", "LOWER" };
+            var List_Of_Possible_Responses_For_Guess_Higher = new List<string>() { "h", "H", "higher", "Higher", "HIGHER" };
 
-            while (userResponse == "h")
+            while (List_Of_Possible_Responses_For_Guess_Higher.Contains(userResponse))
             {
                 numberRangeMinimum = systemGuess;
                 systemGuess = (numberRangeMaximum + numberRangeMinimum) / 2;
-                Console.WriteLine($@"Is you number {systemGuess}? Please type ""y"" if so, otherwise, type ""h"" if your number is higher or ""l"" if your number is lower.");
+                Console.WriteLine($"Is your number {systemGuess}?");
                 userResponse = Console.ReadLine();
 
-                while (userResponse == "l")
+                while (List_Of_Possible_Responses_For_Guess_Lower.Contains(userResponse))
                 {
                     numberRangeMaximum = systemGuess;
                     systemGuess = (numberRangeMaximum + numberRangeMinimum) / 2;
-                    Console.WriteLine($@"Is you number {systemGuess}? Please type ""y"" if so, otherwise, type ""h"" if your number is higher or ""l"" if your number is lower.");
+                    Console.WriteLine($"Is your number {systemGuess}?");
                     userResponse = Console.ReadLine();
                 }
                 if (affirmativeUserResponse.Contains(userResponse))
                 {
                     Console.WriteLine("I'm sure you could do this too, but likely not as fast. ☺️");
                 }
-                // if (userResponse != 
-                // {
-                //     Console.WriteLine("Please enter an acceptable response.");
-                // }
+                else
+                {
+                    Console.WriteLine("Please enter an acceptable response.");
+                }
             }
         }
         static string Greeting()
         {
-            Console.WriteLine();
-            Console.WriteLine();
+            Console.WriteLine("\n");
             Console.WriteLine("**************************************************************************************");
             Console.WriteLine("**************************************************************************************");
             Console.WriteLine("                      WELCOME TO THE ALMIGHTY NUMBER GUESSER!                          ");
             Console.WriteLine("**************************************************************************************");
             Console.WriteLine("**************************************************************************************");
-            Console.WriteLine();
-            Console.WriteLine();
+            Console.WriteLine("\n");
             Console.WriteLine("Hello!");
             Console.WriteLine();
             Console.WriteLine("My goal is to guess a number that you have chosen in your mind.");
